2732e67d6ed1f1669de8ada7528232d1ff1768a2
KAC CHAN <kacxxchan@gmail.com>
2022-10-25 12:32:57 +0530

    Update handlers.py

--- ShogunXRobot/modules/helper_funcs/handlers.py
+++ ShogunXRobot/modules/helper_funcs/handlers.py
@@ -12,9 +12,9 @@ import ShogunXRobot.modules.sql.blacklistusers_sql as sql
 from ShogunXRobot import ALLOW_EXCL, DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
 
 if ALLOW_EXCL:
-    CMD_STARTERS = ("/", "+")
+    CMD_STARTERS = ("/")
 else:
-    CMD_STARTERS = ("/", "+")
+    CMD_STARTERS = ("/")
 
 
 class AntiSpam:
